580d3287b250b6a0af8e735e119230bf6935b2a1
AlthafHussainK <althafakm@gmail.com>
2021-05-07 07:19:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useRef, forwardRef, useEffect, createElement } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+ 
+  return (
+    <section>
+      <h2>Login / Sign up</h2>
+    </section>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
