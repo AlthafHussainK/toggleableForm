106a7339c8114c43ab114a179f94e3454553a2e8
AlthafHussainK <althafakm@gmail.com>
2021-05-07 07:36:14 +0530

    create FormTOggle, ButtonToggle

--- src/index.js
+++ src/index.js
@@ -2,12 +2,66 @@ import React, { useState, useRef, forwardRef, useEffect, createElement } from 'r
 import ReactDOM from 'react-dom';
 
 const App = () => {
- 
+  let data = [
+    { name: 'Log in', component: LoginForm },
+    { name: 'Sign up', component: SignupForm }
+  ]
+
   return (
     <section>
       <h2>Login / Sign up</h2>
     </section>
   )
 }
+
+const ToggleableForm = ({ options }) => {
+  const [currentForm, setCurrentForm ] = useState(0)
+  let focusRef = useRef(null)
+
+  return <>
+    {options.map((el, index) => {
+      return (
+        <ButtonToggle 
+          key={`button${index}`} 
+          toggleForm={() => {
+            setCurrentForm(index)
+          }}
+        >
+          {el.name}
+        </ButtonToggle>
+      )
+    })}
+    <FormToggle currentIndex={currentForm}>
+      {options.map((el, index) => {
+        return (
+          <div key={`form${index}`}>
+            {createElement(el.component, { ref: focusRef })}
+          </div>
+        )
+      })}
+    </FormToggle>
+  </>
+}
+
+const ButtonToggle = ({ children, toggleRef, toggleForm }) => {
+  return (
+    <button onClick={() => {
+      toggleForm()
+    }}>
+      {children}
+    </button>
+  )
+}
+
+const FormToggle = ({ children, currentIndex }) => {
+  if (Array.isArray(children)) {
+    return <div>{children[currentIndex]}</div>
+  }
+  return null
+}
+
+const LoginForm = forwardRef((props, ref) => {
+  
+})
 
 ReactDOM.render(<App />, document.getElementById("root"));
