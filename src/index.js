1f4dc98082f1fc8c6d867f6cda9a5e85b5bc03b9
AlthafHussainK <althafakm@gmail.com>
2021-05-07 07:47:25 +0530

    create LoginForm, SignupForm

--- src/index.js
+++ src/index.js
@@ -61,7 +61,45 @@ const FormToggle = ({ children, currentIndex }) => {
 }
 
 const LoginForm = forwardRef((props, ref) => {
-  
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    ref.current.focus()
+  }, [])
+
+  return <>
+    <input type="text" value={username} ref={ref} placeholder="Username" onChange={(e) => {
+      setUsername(e.target.value)
+    }} />
+    <input type="password" value={password} placeholder="Password" onChange={(e) => {
+      setPassword(e.target.value)
+    }} />
+    <button>Submit</button>
+  </>
+})
+
+const SignupForm = forwardRef((props, ref) => {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    ref.current.focus()
+  }, [])
+
+  return <>
+    <input type="email" value={email} ref={ref} placeholder="Email" onChange={(e) => {
+      setEmail(e.target.value)
+    }} />
+    <input type="text" value={username} placeholder="Username" onChange={(e) => {
+      setUsername(e.target.value)
+    }} />
+    <input type="password" value={password} placeholder="Password" onChange={(e) => {
+      setPassword(e.target.value)
+    }} />
+    <button>Submit</button>
+  </>
 })
 
 ReactDOM.render(<App />, document.getElementById("root"));
